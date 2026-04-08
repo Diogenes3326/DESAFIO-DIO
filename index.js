63953761074fcdc2e4ec044124192db954de8a45
Diogenes3326 <diogenes_dp@hotmail.com>
2026-04-08 11:55:43 +0100

    Minha Atualização

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-let nickname = "Sub  Zero Mestre do Gelo "
+let herois = [
+    {nome: "Gohan", xp: 5000},
+    {nome: "Goku", xp: 10000},
+    {nome: "Vegeta", xp: 9000},
+    {nome: "Goten", xp: 1500},
+     {nome: "Trunks", xp: 1500}
+];
 
-let experienciaXpDeUmHeroi = 
+for (let i = 0; i < herois.length; i++){
 
- 
-console.log ("Bem vindo" + nickname)
+    let nome = herois[i].nome;
+    let xp = herois[i].xp;
+    let nivel = "";
+
+    if (xp <= 1000) {
+        nivel = "Ferro";
+    } else if (xp <= 2000){ 
+        nivel = "Bronze";
+    } else if (xp <=5000){
+        nivel = "Prata";
+    } else if (xp <= 7000){
+        nivel = "Ouro";
+    } else if (xp <= 8000){
+        nivel = "Platina";
+    } else if (xp <= 9000){
+        nivel = "Ascendente"; 
+    } else if (xp <= 10000){
+        nivel = "Radiante"; 
+    }
+
+    console.log(`O Heroi de nome ${nome} está no nivel de ${nivel}`);
+}
+
+
+
 
